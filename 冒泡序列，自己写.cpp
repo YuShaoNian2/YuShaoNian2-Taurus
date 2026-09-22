ca09877441b1eccce1c9e29e23e471f5000aaff3
YuShaoNian2 <3576625096@QQ.com>
2026-09-22 12:48:55 +0800

    Create 冒泡序列，自己写.cpp
    第一次加入exam1文件

---
+++ 冒泡序列，自己写.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int arr[10] = { 5,8,2,10,7,15,13,11,20,18 };
+	int len = sizeof(arr) / sizeof(arr[0]);
+	for (int i = 0;i < len - 1;i++) {
+		for (int j = 0;j < len - 1 - i;j++) {
+			if (arr[j] > arr[j + 1]) {
+				int temp = arr[j];
+				arr[j] = arr[j + 1];
+				arr[j + 1] = temp;
+			}
+		}
+	}
+	for (int i = 0;i < len;i++) {
+		cout << arr[i] << endl;
+		cout <<"ok了兄弟们，晚"<<endl;
+	}
+}
