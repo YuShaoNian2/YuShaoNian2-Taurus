9689b2b166cccb363212fec52c2754036a5d7772
YuShaoNian2 <3576625096@QQ.com>
2026-09-22 15:02:11 +0800

    111

--- bubble_sort.cpp
+++ bubble_sort.cpp
@@ -3,6 +3,7 @@ using namespace std;
 
 int main()
 {
+	system("chcp 65001");
 	int arr[10] = { 5,8,2,10,7,15,13,11,20,18 };
 	int len = sizeof(arr) / sizeof(arr[0]);
 	for (int i = 0;i < len - 1;i++) {
@@ -16,6 +17,7 @@ int main()
 	}
 	for (int i = 0;i < len;i++) {
 		cout << arr[i] << endl;
-		cout <<"ok了兄弟们，我终于完成云上传了"<<endl;
+		cout <<"ok了兄弟们，我终于完成云上传了哈哈"<<endl;
 	}
+	return 0;
 }
